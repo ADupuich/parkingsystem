d3a4296d3ad0a6d383a8f3f9cde8ab05b313c826
Amaury Dupuich <amaury.dupuich@live.fr>
2021-07-22 10:12:08 +0200

    #2 commit
    pom xml mis à jour, TODO 1 et 2 validée

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -118,7 +118,7 @@ public class FareCalculatorServiceTest {
 	@Test
 	public void calculateFareCarWithMoreThanADayParkingTime() {
 		Date inTime = new Date();
-		inTime.setTime(System.currentTimeMillis() - (24 * 60 * 60 * 1000));// 24 hours parking time should give 24 *
+		inTime.setTime(System.currentTimeMillis() - (25 * 60 * 60 * 1000));// 24 hours parking time should give 24 *
 																			// parking fare per hour
 		Date outTime = new Date();
 		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
@@ -127,7 +127,7 @@ public class FareCalculatorServiceTest {
 		ticket.setOutTime(outTime);
 		ticket.setParkingSpot(parkingSpot);
 		fareCalculatorService.calculateFare(ticket);
-		assertEquals((24 * Fare.CAR_RATE_PER_HOUR), ticket.getPrice());
+		assertEquals((25 * Fare.CAR_RATE_PER_HOUR), ticket.getPrice());
 	}
 
 }
